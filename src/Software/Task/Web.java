359f34d5facc1926e4fb2fcac860c6668e1a60af
Itay2805 <itay2805.il@gmail.com>
2015-01-11 19:25:40 +0200

    Create Web.java

---
+++ src/Software/Task/Web.java
@@ -0,0 +1,19 @@
+package Software.Task;
+
+import Hardware.Computer;
+
+public class Web extends Task{
+
+/*
+*
+*Work in progress
+*
+*/
+
+	public Web(Computer computer, int minUsage,
+			int space) {
+		super(computer, "Browser", "exe", minUsage, space, true);
+	}
+	
+
+}
