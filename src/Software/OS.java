d8208132b3a9f50f9e682373d231b4274e295307
Itay2805 <itay2805.il@gmail.com>
2015-01-11 19:22:39 +0200

    Create OS.java

---
+++ src/Software/OS.java
@@ -0,0 +1,55 @@
+package Software;
+
+import java.util.Scanner;
+
+import toolBox.Print;
+
+public class OS {
+	
+	public String name;
+	public String companyName;
+	
+	public int basicSpace;
+	public int basicUsage;
+	
+	public Scanner reader = new Scanner(System.in);
+	
+	public OS(int basicSpace, int basicUsage, String name, String companyName) {
+		this.name = name;
+		this.companyName = companyName;
+		this.basicSpace = basicSpace;
+		this.basicUsage = basicUsage;
+	}
+	
+	public String GUI() {
+		Print.info("---------------Welcome to " + name + " OS!---------------");
+		Print.info("Choose an action(Type): ");
+		Print.info("Info(info) - Computer Info");
+		Print.info("Wallet(wallet) - Bank Account info");
+		Print.info("Console(console) - opens the console!");
+		Print.info("Shtdown(shutdown) - Shuts down the computer");
+		Print.info("IInstall (web) - install .prog files!");
+		Print.info("---------------------------------------------------------");
+		String action = reader.next();
+		return action;
+	}
+	
+	/** Getters**/
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public int getBasicUsage() {
+		return basicUsage;
+	}
+	
+	public int getbasicSpace() {
+		return basicSpace;
+	}
+
+}
