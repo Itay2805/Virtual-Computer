3311c0fdc8973ef24e8e166f6434c08542a87200
Itay2805 <itay2805.il@gmail.com>
2015-01-14 22:16:24 +0200

    Revert "Update ErrorTypes.java"
    This reverts commit d49f4c5dd4f2714a5bcbb448b1c7b68a93fa272f.

--- src/Error/ErrorTypes.java
+++ src/Error/ErrorTypes.java
@@ -4,23 +4,13 @@ import Hardware.HD;
 import Hardware.RAM;
 import toolBox.Print;
 
-public class ErrorTypes{
+public class Error{
 	
 	public boolean NO_MEMORY_FOR_SOFTWARE = false;
 	public boolean NO_RAM_FOR_TASK = false;
 	
 	public static boolean NO_MEMORY_FOR_SOFTWARE(HD hd, int minSpace){
 		if(hd.getTotalSpace() - hd.getUsedSpase() < minSpace) {
-			Print.info("---------------------------------------------------");
-			Print.info("ERROR Type:  NO_MEMORY_FOR_SOFTWARE");
-			Print.info("ERROR Info:  Total  space: " + hd.getTotalSpace() + "MB");
-			Print.info("             Used   space: " + hd.getUsedSpase() + "MB");
-			Print.info("             Needed space: " + minSpace);
-			Print.info("---------------------------------------------------");
-			Print.info("Error happendes when the computer doesn't have");
-			Print.info("enoght Memory to download a software! Upgrade");
-			Print.info(" your HD or delete unneeded softwares!");
-			Print.info("---------------------------------------------------");
 			return true;
 		}
 		return false;
