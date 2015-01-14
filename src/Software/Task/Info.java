7d308ebd0162565af5e5e5e59fe5f8e0da7d4ea3
Itay2805 <itay2805.il@gmail.com>
2015-01-14 21:20:05 +0200

    Update Info.java

--- src/Software/Task/Info.java
+++ src/Software/Task/Info.java
@@ -3,6 +3,7 @@ package Software.Task;
 import java.util.Scanner;
 
 import toolBox.Print;
+import Error.ErrorTypes;
 import Hardware.Computer;
 
 public class Info extends Task{
@@ -14,27 +15,12 @@ public class Info extends Task{
 	}
 	
 	public void start() {
-		if(computer.getRam().getTotalRam() - computer.getRam().getUsedRAM() > minUsage && computer.on) {
-			Print.info("Starting " + (computer.getOs().getName() + ".task.start.info.exe"));
+		if(!ErrorTypes.NO_RAM_FOR_TASK(computer.getRam(), minUsage) && computer.on) {
+			if(computer.ROOT) {
+				Print.info("Starting " + (computer.getOs().getName() + ".task.start.info.exe"));
+			}
 			computer.getRam().addUsedRAM(minUsage);
 			task();
-		} else {
-			if (computer.on) {
-				Print.info("---------------------------------------------------");
-				Print.info("Error when trying to start: " + (computer.getOs().getName() + ".task.start.info.exe"));
-				Print.info("---------------------------------------------------");
-				Print.info("ERROR Type:  NO_RAM_FOR_TASK");
-				Print.info("ERROR Info:  Total  RAM: " + computer.getRam().getTotalRam());
-				Print.info("             Used   RAM: " + computer.getRam().getUsedRAM());
-				Print.info("             Needed RAM: " + minUsage);
-				Print.info("---------------------------------------------------");
-				Print.info("Error happendes when the computer doesn't have");
-				Print.info("enoght RAM to run an task! Upgrade your RAM or");
-				Print.info("stop unused tasks to avoid getting the error! ");
-				Print.info("---------------------------------------------------");
-			} else {
-				Print.info("Coul'd not load task! Computer is not turned on!");
-			}
 		}
 	}
 	
@@ -58,7 +44,9 @@ public class Info extends Task{
 	}
 	
 	public void stop() {
-		Print.info("Starting " + (computer.getOs().getName() + ".task.kill.info.exe"));
+		if(computer.ROOT) {
+			Print.info("Starting " + (computer.getOs().getName() + ".task.kill.info.exe"));
+		}
 		computer.getRam().addUsedRAM(-minUsage);
 	}
 	
